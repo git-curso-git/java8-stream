ff9f0af4ce9b8d990377eaaf437333126c1d51ea
GIT CURSO <git.curso.git@gmail.com>
2019-07-09 20:57:21 +0200

    Proceso con stream

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,7 +1,7 @@
 package hello;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+	
+	final int MIN_SIZE = 99;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -23,10 +25,60 @@ public class Application {
 
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
+            
+            int totGTsize = 0;
             for (String beanName : beanNames) {
-                System.out.println(beanName);
+            	int beanLength = beanName.length();
+
+            	if(beanLength > MIN_SIZE) {
+            		totGTsize ++;
+            	}
+                System.out.format(
+                		"%6d %4s %4s %s\n", 
+                		beanLength, 
+                		(beanLength > MIN_SIZE?" SI ":"    "),
+                		(beanLength > MIN_SIZE?String.valueOf(totGTsize):"    "),
+                		beanName
+                );
             }
 
+            int totBeans = beanNames.length;
+            System.out.format("Beans : %05d/%d\n", totBeans, totGTsize);
+            
+            System.out.println();
+            System.out.println("Calcular usando stream");
+            int totBeansStream = (int) Arrays.asList(beanNames)
+            	.stream()
+            	.filter((it) -> it.length() > MIN_SIZE)
+            	.count();
+            
+            System.out.format(">%d hay : %d\n", MIN_SIZE, totBeansStream);
+            
+            System.out.println();
+            System.out.println("Calcular usando stream, con reduction mediante refMethod");
+            int totBeansStreamRM = (int) Arrays.asList(beanNames)
+            	.parallelStream()
+            	.mapToInt(String::length)
+            	.filter((it) -> it > MIN_SIZE)
+            	.count();
+            
+            System.out.format(">%d hay : %d", MIN_SIZE, totBeansStreamRM);
+            
+            System.out.println();
+            System.out.println("Obtener los valores en un array");
+            int[] listaSize = Arrays.asList(beanNames)
+                	.parallelStream()
+                	.mapToInt(String::length)
+                	.filter((it) -> it > MIN_SIZE)
+                	.sorted()
+                	.toArray();
+            
+            System.out.println();
+            System.out.println("Elementos : ");
+            for(int s : listaSize) {
+            	System.out.print(s + ", ");
+            }
+            System.out.println();
         };
     }
 
